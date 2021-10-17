f56947c4363f86e0c249cb7c0fb5be74960e5c22
walbert chaves <71409787+walbinh0@users.noreply.github.com>
2021-10-17 17:46:04 -0300

    mv back

---
+++ README.md
@@ -0,0 +1,3 @@
+# O que eu consegui fazer em back
+
+infelizmente não consegui fazer com que o front e o back se respondessem e criar um banco de dados mas a estrutura do back é essa aqui e juntamente a ela aprendi uma noção de usar o heroku
--- src/main/java/com/example/demo/Controler.java
+++ src/main/java/com/example/demo/Controler.java
@@ -28,37 +28,12 @@ public class UserController {
   @Autowired
   private UserRepository userRepository;
 
-  @GetMapping("/{id}")
-  public User user(@PathVariable("id") Long id) {
+  @GetMapping("/")
+  public String user() {
 
-    Optional<User> userFind = this.userRepository.findById(id);
-
-    if (userFind.isPresent()) {
-      return userFind.get();
-    }
-
-    return null;
+    return "hello world";
   }
 
-  @PostMapping("/")
-  public User user(@RequestBody User user) {
-    return this.userRepository.save(user);
 
-  }
-
-  @GetMapping("/list")
-  public List<User> list() {
-    return this.userRepository.findAll();
-  }
-
-  @GetMapping("/list/{id}")
-  public List<User> listMoreThan(@PathVariable("id") Long id) {
-    return this.userRepository.findByIdGreaterThan(id);
-  }
-
-  @GetMapping("/findByName/{name}")
-  public List<User> findByName(@PathVariable("name") String name) {
-    return this.userRepository.findByNameIgnoreCase(name);
-  }
 
 }
